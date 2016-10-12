9b55d0ad8f9fb0876ade5228c3642b3a4ae04677
Juan Daniel Flores <juandfloresm@gmail.com>
2016-10-12 13:04:13 -0400

    Centring on DC

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
+var map = L.map('map').setView([38.889931, -77.009003], 13);
 
 L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
